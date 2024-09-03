4f7ae0f29167a074e943b20a0efea40397f7c53e
Pablo Gonzalez <pgosorio13@gmail.com>
2024-09-03 13:51:38 -0500

    dev branch init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SIGIM-Mob
+#Integrantes:
+
+- Jimmy Alexander Giraldo Montoya
+- Valentina Morales Sanchez
+- Juan Pablo Gonzalez Osorio
---
+++ styles/common.styles.ts
@@ -0,0 +1,11 @@
+const colorPalette = {
+  primary: '#BBB',
+  secondary: '#EEE',
+  success: '#00BCD4',
+  warning: '#FF9800',
+  danger: '#F44336',
+  light: '#F5F5F5',
+  dark: '#212121',
+};
+
+export { colorPalette };
