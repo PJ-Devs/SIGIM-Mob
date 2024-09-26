adc5e2e0ff5a154d7ca34fcbe5fd60178abc507a
ValenMorales <valentina.morales@kommit.co>
2024-09-26 16:23:44 -0500

    login fetch

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ export const registerEnterprise = async (body:any) => {
   console.log('empece')
   console.log(body)
     try {
-      const response = await fetch('http://192.168.105.9:8000/api/auth/signup', {
+      const response = await fetch('http://192.168.105.9:8000/api/auth/token', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -19,3 +19,25 @@ export const registerEnterprise = async (body:any) => {
       console.error('Error registrando la empresa:', error);
     }
   };
+
+  export const login = async (body:any) => {
+    console.log('empece')
+    console.log(body)
+      try {
+        const response = await fetch('http://192.168.105.9:8000/api/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          body: JSON.stringify(body),
+        });
+        
+        const updatedEnterprise = await response.json();
+        console.log('Empresa registrada:', updatedEnterprise);
+        return updatedEnterprise;
+      } catch (error) {
+        console.error('Error registrando la empresa:', error);
+      }
+    };
+  
