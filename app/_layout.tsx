2592aadb78a29211b86459cc799dca7ef622617d
valen <valentina.morales@kommit.co>
2024-11-25 02:08:45 -0500

    Push notifications configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,45 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
+import messaging from "@react-native-firebase/messaging";
+import { useEffect } from "react";
 import { SQLiteProvider } from "expo-sqlite";
 import {
   authInterceptor,
   errorInterceptor,
 } from "../lib/axios/axios.interceptors";
 import { initializeDB } from "../lib/sqlite";
+import { requestUserPermission } from "../lib/notifications";
+import Toast from "react-native-toast-message";
 
 authInterceptor();
 errorInterceptor();
 
 export default function layout () {
+
+  useEffect(() => {
+    requestUserPermission();
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("new notification background", remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Toast.show({
+        type: "info",
+        text1: remoteMessage.notification?.title ?? "Nuevo mensaje",
+        text2: remoteMessage.notification?.body ?? "Nuevo mensaje",
+        visibilityTime: 3000,
+        swipeable: true,
+        text1Style: { fontSize: 16 },
+        text2Style: { fontSize: 14 },
+        topOffset: 60,
+      });
+    });
+
+    return unsubscribe;
+
+  }, []);
+
   return (
     <SQLiteProvider databaseName="test.db" onInit={initializeDB}>
       <AuthProvider>
--- lib/axios/axios.config.ts
+++ lib/axios/axios.config.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_URL = 'http://192.168.1.6:8000/api';
 
-
 const APIInstance = axios.create({
   baseURL: API_URL,
   timeout: 10000,
